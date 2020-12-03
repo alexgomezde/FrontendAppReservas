88cad4828db963711880c96929e9d5faced8563c
Adrian <adriancampos2001@gmail.com>
2020-12-03 15:10:53 -0600

    Incluidos los models y manager de Asiento y Factura
    Aún me falta terminar todo lo referente a los views

---
+++ AppReservasSW/Controllers/AsientoManager.cs
@@ -0,0 +1,65 @@
+﻿using AppReservasSW.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace AppReservasSW.Controllers
+{
+    public class AsientoManager
+    {
+        const string URL = "http://localhost:49220/api/asiento/";
+        const string URLIngresar = "http://localhost:49220/api/asiento/ingresar/";
+
+        HttpClient GetClient(string token)
+        {
+            HttpClient client = new HttpClient();
+
+            client.DefaultRequestHeaders.Add("Authorization", token);
+            client.DefaultRequestHeaders.Add("Accept", "application/json");
+
+            return client;
+        }
+        public async Task<IEnumerable<Asiento>> ObtenerAsientos(string token)
+        {
+            HttpClient client = GetClient(token);
+            string resultado = await client.GetStringAsync(URL);
+
+            return JsonConvert.DeserializeObject<IEnumerable<Asiento>>(resultado);
+        }
+
+        public async Task<IEnumerable<Asiento>> ObtenerAsiento(string token, string codigo)
+        {
+            HttpClient client = GetClient(token);
+            string resultado = await client.GetStringAsync(URL + codigo);
+            return JsonConvert.DeserializeObject<IEnumerable<Asiento>>(resultado);
+        }
+        public async Task<Asiento> Ingresar(Asiento asiento, string token)
+        {
+            HttpClient client = GetClient(token);
+            var response = await client.PostAsync(URLIngresar,
+                new StringContent(JsonConvert.SerializeObject(asiento), Encoding.UTF8,
+                "application/json"));
+            return JsonConvert.DeserializeObject<Asiento>(await response.Content.ReadAsStringAsync());
+        }
+        public async Task<Asiento> Actualizar(Asiento asiento, string token)
+        {
+            HttpClient client = GetClient(token);
+            var response = await client.PutAsync(URL,
+                new StringContent(JsonConvert.SerializeObject(asiento), Encoding.UTF8,
+                "application/json"));
+            return JsonConvert.DeserializeObject<Asiento>(await response.Content.ReadAsStringAsync());
+        }
+        public async Task<string> Eliminar(string codigo, string token)
+        {
+            HttpClient client = GetClient(token);
+            var response = await client.DeleteAsync(URL + codigo);
+
+            return JsonConvert.DeserializeObject<string>(await response.Content.ReadAsStringAsync());
+        }
+    }
+}
---
+++ AppReservasSW/Controllers/FacturaManager.cs
@@ -0,0 +1,65 @@
+﻿using AppReservasSW.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace AppReservasSW.Controllers
+{
+    public class FacturaManager
+    {
+        const string URL = "http://localhost:49220/api/factura/";
+        const string URLIngresar = "http://localhost:49220/api/factura/ingresar/";
+
+        HttpClient GetClient(string token)
+        {
+            HttpClient client = new HttpClient();
+
+            client.DefaultRequestHeaders.Add("Authorization", token);
+            client.DefaultRequestHeaders.Add("Accept", "application/json");
+
+            return client;
+        }
+        public async Task<IEnumerable<Factura>> ObtenerFacturas(string token)
+        {
+            HttpClient client = GetClient(token);
+            string resultado = await client.GetStringAsync(URL);
+
+            return JsonConvert.DeserializeObject<IEnumerable<Factura>>(resultado);
+        }
+
+        public async Task<IEnumerable<Factura>> ObtenerFactura(string token, string codigo)
+        {
+            HttpClient client = GetClient(token);
+            string resultado = await client.GetStringAsync(URL + codigo);
+            return JsonConvert.DeserializeObject<IEnumerable<Factura>>(resultado);
+        }
+        public async Task<Factura> Ingresar(Factura factura, string token)
+        {
+            HttpClient client = GetClient(token);
+            var response = await client.PostAsync(URLIngresar,
+                new StringContent(JsonConvert.SerializeObject(factura), Encoding.UTF8,
+                "application/json"));
+            return JsonConvert.DeserializeObject<Factura>(await response.Content.ReadAsStringAsync());
+        }
+        public async Task<Factura> Actualizar(Factura factura, string token)
+        {
+            HttpClient client = GetClient(token);
+            var response = await client.PutAsync(URL,
+                new StringContent(JsonConvert.SerializeObject(factura), Encoding.UTF8,
+                "application/json"));
+            return JsonConvert.DeserializeObject<Factura>(await response.Content.ReadAsStringAsync());
+        }
+        public async Task<string> Eliminar(string codigo, string token)
+        {
+            HttpClient client = GetClient(token);
+            var response = await client.DeleteAsync(URL + codigo);
+
+            return JsonConvert.DeserializeObject<string>(await response.Content.ReadAsStringAsync());
+        }
+    }
+}
