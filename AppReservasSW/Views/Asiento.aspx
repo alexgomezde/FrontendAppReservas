f1c9fb75a4d55abbe30baec09c15228eb11feef3
Adrian <adriancampos2001@gmail.com>
2020-12-04 22:24:52 -0600

    Agregué el view de Asiento
    Me falta el de factura y si es posible una comprobación en caso de repetir asientos

---
+++ AppReservasSW/Views/Asiento.aspx
@@ -0,0 +1,101 @@
+﻿<%@ Page Title="" Async="true" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Asiento.aspx.cs" Inherits="AppReservasSW.Views.Asiento" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+    <asp:GridView ID="grdAsientos" CssClass="table table-dark table-sm text-center mt-1" runat="server" CellPadding="4" ForeColor="#333333" GridLines="None"  AutoGenerateColumns="False"
+         OnRowDeleting="grdAsientos_RowDeleting" OnRowEditing="grdAsientos_RowEditing" OnRowCancelingEdit="grdAsientos_RowCancelingEdit"
+         OnRowUpdating="grdAsientos_RowUpdating" OnRowDataBound="grdAsientos_RowDataBound">
+        <AlternatingRowStyle BackColor="White" ForeColor="#284775" />
+        <EditRowStyle BackColor="#999999" />
+        <FooterStyle BackColor="#5D7B9D" Font-Bold="True" ForeColor="White" />
+        <HeaderStyle BackColor="#5D7B9D" Font-Bold="True" ForeColor="White" />
+        <PagerStyle BackColor="#284775" ForeColor="White" HorizontalAlign="Center" />
+        <RowStyle BackColor="#F7F6F3" ForeColor="#333333" />
+        <SelectedRowStyle BackColor="#E2DED6" Font-Bold="True" ForeColor="#333333" />
+        <SortedAscendingCellStyle BackColor="#E9E7E2" />
+        <SortedAscendingHeaderStyle BackColor="#506C8C" />
+        <SortedDescendingCellStyle BackColor="#FFFDF8" />
+        <SortedDescendingHeaderStyle BackColor="#6F8DAE" />
+
+        <Columns>
+            <asp:TemplateField HeaderText="Código">
+                <ItemTemplate>
+                    <asp:Label ID="lblCodigoAsiento" Text='<%# Eval("ASI_CODIGO") %>' runat="server" />
+                </ItemTemplate>
+            </asp:TemplateField>
+
+            <asp:TemplateField HeaderText="Letra Asiento">
+                <ItemTemplate>
+                    <%# Eval("ASI_LETRA") %>
+                </ItemTemplate>
+                <EditItemTemplate>
+                    <asp:TextBox ID="txtLetraAsientoEdit" Text='<%# Eval("ASI_LETRA")%>' runat="server" />
+                </EditItemTemplate>
+            </asp:TemplateField>
+
+            <asp:TemplateField HeaderText="Fila Asiento">
+                <ItemTemplate>
+                    <%# Eval("ASI_FILA") %>
+                </ItemTemplate>
+                <EditItemTemplate>
+                    <asp:TextBox ID="txtFilaAsientoEdit" Text='<%# Eval("ASI_FILA")%>' runat="server" />
+                </EditItemTemplate>
+            </asp:TemplateField>
+
+            <asp:TemplateField HeaderText="Estado Asiento">
+                <ItemTemplate>
+                    <asp:Label ID="lblCategoria" Text='<%# Eval("ASI_ESTADO") %>' runat="server" CssClass="GridViewEditRow" />
+                </ItemTemplate>
+                <EditItemTemplate>
+                    <asp:DropDownList ID="drpDisponibilidadEdit" runat="server">
+                        <asp:ListItem Value="D">Disponible</asp:ListItem>
+                        <asp:ListItem Value="N">No Disponible</asp:ListItem>
+                    </asp:DropDownList>
+                </EditItemTemplate>
+                <FooterTemplate>
+                    <asp:TextBox ID="txtHotCategoriaFooter" runat="server" />
+                </FooterTemplate>
+            </asp:TemplateField>
+
+            <asp:TemplateField HeaderText="Acciones">
+                <ItemTemplate>
+                    <asp:ImageButton ImageUrl="/Images/edit_icon.png" runat="server" CommandName="Edit" ToolTip="Editar" type="button" />
+                    <asp:ImageButton ImageUrl="/Images/trash_icon.png" runat="server" CommandName="Delete" ToolTip="Eliminar" type="button" />
+                </ItemTemplate>
+                <EditItemTemplate>
+                    <asp:ImageButton ImageUrl="/Images/check.png" runat="server" CommandName="Update" ToolTip="Actualizar" />
+                    <asp:ImageButton ImageUrl="/Images/close.png" runat="server" CommandName="Cancel" ToolTip="Cancelar" />
+                </EditItemTemplate>
+            </asp:TemplateField>
+
+        </Columns>
+
+    </asp:GridView>
+
+    <div class="col-md-6">
+
+        <div class="form-inline mb-3">
+            <asp:Label ID="Label1" runat="server" Text="Letra Asiento" CssClass="col-form-label col-md-4"></asp:Label>
+            <asp:TextBox ID="txtLetraAsiento" runat="server" CssClass="form-control col-md-8"></asp:TextBox>
+        </div>
+
+        <div class="form-inline mb-3">
+            <asp:Label ID="Label3" runat="server" Text="Fila Asiento" CssClass="col-form-label col-md-4"></asp:Label>
+            <asp:TextBox ID="txtFilaAsiento" runat="server" CssClass="form-control col-md-8"></asp:TextBox>
+        </div>
+
+        <div class="form-inline mb-3">
+            <asp:Label  runat="server" Text="Estado" CssClass="col-form-label col-md-4"></asp:Label>
+            <asp:DropDownList ID="drpDisponibilidad" runat="server" CssClass="form-control col-md-8">
+                <asp:ListItem Selected="True" Value="D">Disponible</asp:ListItem>
+                <asp:ListItem Value="N">No Disponible</asp:ListItem>
+             </asp:DropDownList>
+        </div>
+
+        <div class="form-inline mt-3 mb-1">
+            <asp:Button ID="btnAgregar" CssClass="btn btn-primary col-md-12 mt-3" runat="server" Text="Agregar" OnClick="btnAgregar_Click" />
+            <asp:Label ID="lblStatus" runat="server" Text="Label" ForeColor="#006600" Visible="False"></asp:Label>
+        </div>
+
+    </div>
+</asp:Content>
---
+++ AppReservasSW/Views/Asiento.aspx.cs
@@ -0,0 +1,243 @@
+﻿using AppReservasSW.Controllers;
+using Microsoft.Ajax.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+
+namespace AppReservasSW.Views
+{
+    public partial class Asiento : System.Web.UI.Page
+    {
+        IEnumerable<Models.Asiento> asientos = new ObservableCollection<Models.Asiento>();
+        AsientoManager asientoManager = new AsientoManager();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                InicializarControles();
+            }
+        }
+
+        private async void InicializarControles()
+        {
+
+            asientos = await asientoManager.ObtenerAsientos(VG.usuarioActual.CadenaToken);
+            grdAsientos.DataSource = asientos.ToList();
+            grdAsientos.DataBind();
+        }
+
+        protected async void btnAgregar_Click(object sender, EventArgs e)
+        {
+            if (ValidarInsertar())
+            {
+                Models.Asiento asientoIngresado = new Models.Asiento();
+                Models.Asiento asiento = new Models.Asiento()
+                {
+
+                    ASI_LETRA = txtLetraAsiento.Text,
+                    ASI_FILA = Convert.ToInt32(txtFilaAsiento.Text),
+                    ASI_ESTADO = drpDisponibilidad.SelectedValue.ToString()
+                };
+
+                asientoIngresado =
+                    await asientoManager.Ingresar(asiento, VG.usuarioActual.CadenaToken);
+
+                if (asientoIngresado != null)
+                {
+                    lblStatus.Text = "Asiento ingresado correctamente";
+                    lblStatus.ForeColor = Color.Green;
+                    lblStatus.Visible = true;
+                    InicializarControles();
+                }
+                else
+                {
+                    lblStatus.Text = "Hubo un error al ingresar el asiento";
+                    lblStatus.ForeColor = Color.Maroon;
+                    lblStatus.Visible = true;
+                }
+            }
+        }
+
+        protected async void grdAsientos_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            string codigoAsientoEliminado = string.Empty;
+            Label lblCode = (Label)grdAsientos.Rows[e.RowIndex].Cells[0].FindControl("lblCodigoAsiento");
+
+            string codigoAsiento = lblCode.Text;
+
+            codigoAsientoEliminado = await asientoManager.Eliminar(codigoAsiento, VG.usuarioActual.CadenaToken);
+
+            if (!string.IsNullOrEmpty(codigoAsientoEliminado))
+            {
+                lblStatus.Text = "Asiento eliminado correctamente";
+                lblStatus.ForeColor = Color.Green;
+                lblStatus.Visible = true;
+                InicializarControles();
+            }
+            else
+            {
+                lblStatus.Text = "Hubo un error al eliminar el asiento";
+                lblStatus.ForeColor = Color.Maroon;
+                lblStatus.Visible = true;
+            }
+
+        }
+
+        protected void grdAsientos_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            grdAsientos.EditIndex = e.NewEditIndex;
+            InicializarControles();
+        }
+
+        protected void grdAsientos_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            grdAsientos.EditIndex = -1;
+            InicializarControles();
+        }
+
+        protected async void grdAsientos_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            Label lblCode = (Label)grdAsientos.Rows[e.RowIndex].Cells[0].FindControl("lblCodigoAsiento");
+
+            string asiLetra = (grdAsientos.Rows[e.RowIndex].FindControl("txtLetraAsientoEdit") as TextBox).Text;
+            string asiFila = (grdAsientos.Rows[e.RowIndex].FindControl("txtFilaAsientoEdit") as TextBox).Text;
+            string asiEstado = (grdAsientos.Rows[e.RowIndex].FindControl("drpDisponibilidadEdit") as DropDownList).Text;
+
+            if (ValidarModificar(asiLetra, asiFila, asiEstado))
+            {
+                Models.Asiento asientoModificado = new Models.Asiento();
+                Models.Asiento asiento = new Models.Asiento()
+                {
+                    ASI_CODIGO = Convert.ToInt32(lblCode.Text),
+                    ASI_LETRA = asiLetra,
+                    ASI_FILA = Convert.ToInt32(asiFila),
+                    ASI_ESTADO =asiEstado
+                };
+
+                asientoModificado =
+                    await asientoManager.Actualizar(asiento, VG.usuarioActual.CadenaToken);
+
+                if (asientoModificado != null)
+                {
+                    lblStatus.Text = "Asiento modificado correctamente";
+                    lblStatus.ForeColor = Color.Green;
+                    lblStatus.Visible = true;
+                    InicializarControles();
+                }
+                else
+                {
+                    lblStatus.Text = "Hubo un error al modificar el asiento";
+                    lblStatus.ForeColor = Color.Maroon;
+                    lblStatus.Visible = true;
+                }
+
+                grdAsientos.EditIndex = -1;
+                InicializarControles();
+            }
+
+        }
+
+        protected void grdAsientos_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+
+        }
+
+        private bool ValidarInsertar()
+        {
+
+            if (txtLetraAsiento.Text.IsNullOrWhiteSpace())
+            {
+                lblStatus.Text = "Debe ingresar la letra del asiento";
+                lblStatus.ForeColor = Color.Maroon;
+                lblStatus.Visible = true;
+                return false;
+            }
+
+            if(txtLetraAsiento.Text.All(char.IsNumber) == true)
+            {
+                lblStatus.Text = "Letra Asiento no puede ser un número";
+                lblStatus.ForeColor = Color.Maroon;
+                lblStatus.Visible = true;
+                return false;
+            }
+
+            if (txtFilaAsiento.Text.IsNullOrWhiteSpace())
+            {
+                lblStatus.Text = "Debe ingresar el número de fila del asiento";
+                lblStatus.ForeColor = Color.Maroon;
+                lblStatus.Visible = true;
+                return false;
+            }
+
+            if (txtFilaAsiento.Text.All(char.IsNumber) == false)
+            {
+                lblStatus.Text = "Fila del asiento debe ser un número";
+                lblStatus.ForeColor = Color.Maroon;
+                lblStatus.Visible = true;
+                return false;
+            }
+
+            if (drpDisponibilidad.SelectedValue.IsNullOrWhiteSpace())
+            {
+                lblStatus.Text = "Debe ingresar el estado del asiento";
+                lblStatus.ForeColor = Color.Maroon;
+                lblStatus.Visible = true;
+                return false;
+            }
+
+            return true;
+        }
+
+        private bool ValidarModificar(string asiLetra, string asiFila, string asiEstado)
+        {
+
+            if (asiLetra.IsNullOrWhiteSpace())
+            {
+                lblStatus.Text = "Debe ingresar la letra del asiento";
+                lblStatus.ForeColor = Color.Maroon;
+                lblStatus.Visible = true;
+                return false;
+            }
+
+            if (asiLetra.All(char.IsNumber) == true)
+            {
+                lblStatus.Text = "Letra Asiento no puede ser un número";
+                lblStatus.ForeColor = Color.Maroon;
+                lblStatus.Visible = true;
+                return false;
+            }
+
+            if (asiFila.IsNullOrWhiteSpace())
+            {
+                lblStatus.Text = "Debe ingresar el número de fila del asiento";
+                lblStatus.ForeColor = Color.Maroon;
+                lblStatus.Visible = true;
+                return false;
+            }
+
+            if (asiFila.All(char.IsNumber) == false)
+            {
+                lblStatus.Text = "Fila del asiento debe ser un número";
+                lblStatus.ForeColor = Color.Maroon;
+                lblStatus.Visible = true;
+                return false;
+            }
+
+            if (asiEstado.IsNullOrWhiteSpace())
+            {
+                lblStatus.Text = "Debe ingresar el estado del asiento";
+                lblStatus.ForeColor = Color.Maroon;
+                lblStatus.Visible = true;
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
