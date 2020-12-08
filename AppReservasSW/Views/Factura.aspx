c656ffa31a9466a26ed35a4ed859ceb1ca6da4e7
Adrian <adriancampos2001@gmail.com>
2020-12-07 23:08:34 -0600

    Agregado Factura

---
+++ AppReservasSW/Views/Factura.aspx
@@ -0,0 +1,102 @@
+﻿<%@ Page Title="" Async="true" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Factura.aspx.cs" Inherits="AppReservasSW.Views.Factura" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+    <asp:GridView ID="grdFacturas" CssClass="table table-dark table-sm text-center mt-1" runat="server" CellPadding="4" ForeColor="#333333" GridLines="None"  AutoGenerateColumns="False"
+         OnRowDeleting="grdFacturas_RowDeleting" OnRowEditing="grdFacturas_RowEditing" OnRowCancelingEdit="grdFacturas_RowCancelingEdit"
+         OnRowUpdating="grdFacturas_RowUpdating" OnRowDataBound="grdFacturas_RowDataBound">
+        <AlternatingRowStyle BackColor="White" ForeColor="#284775" />
+        <EditRowStyle BackColor="#999999" />
+        <FooterStyle BackColor="#5D7B9D" Font-Bold="True" ForeColor="White" />
+        <HeaderStyle BackColor="#5D7B9D" Font-Bold="True" ForeColor="White" />
+        <PagerStyle BackColor="#284775" ForeColor="White" HorizontalAlign="Center" />
+        <RowStyle BackColor="#F7F6F3" ForeColor="#333333" />
+        <SelectedRowStyle BackColor="#E2DED6" Font-Bold="True" ForeColor="#333333" />
+        <SortedAscendingCellStyle BackColor="#E9E7E2" />
+        <SortedAscendingHeaderStyle BackColor="#506C8C" />
+        <SortedDescendingCellStyle BackColor="#FFFDF8" />
+        <SortedDescendingHeaderStyle BackColor="#6F8DAE" />
+
+        <Columns>
+            <asp:TemplateField HeaderText="Código Factura">
+                <ItemTemplate>
+                    <asp:Label ID="lblCodigoFactura" Text='<%# Eval("FAC_CODIGO") %>' runat="server" />
+                </ItemTemplate>
+            </asp:TemplateField>
+
+            <asp:TemplateField HeaderText="Pago Código">
+                <ItemTemplate>
+                    <%# Eval("PAG_CODIGO") %>
+                </ItemTemplate>
+                <EditItemTemplate>
+                    <asp:TextBox ID="txtPagoCodigoEdit" Text='<%# Eval("PAG_CODIGO")%>' runat="server" />
+                </EditItemTemplate>
+            </asp:TemplateField>
+
+            <asp:TemplateField HeaderText="Factura Comprobante">
+                <ItemTemplate>
+                    <%# Eval("FAC_COMPROBANTE") %>
+                </ItemTemplate>
+                <EditItemTemplate>
+                    <asp:TextBox ID="txtFacComprobanteEdit" Text='<%# Eval("FAC_COMPROBANTE")%>' runat="server" CssClass="GridViewEditRow" />
+                </EditItemTemplate>
+            </asp:TemplateField>
+
+            <asp:TemplateField HeaderText="Estado Factura">
+                <ItemTemplate>
+                    <asp:Label ID="lblCategoria" Text='<%# Eval("FAC_ESTADO") %>' runat="server" CssClass="GridViewEditRow" />
+                </ItemTemplate>
+                <EditItemTemplate>
+                    <asp:DropDownList ID="drpDisponibilidadEdit" runat="server">
+                        <asp:ListItem Value="R">Recibida</asp:ListItem>
+                        <asp:ListItem Value="P">Pagada</asp:ListItem>
+                    </asp:DropDownList>
+                </EditItemTemplate>
+                <FooterTemplate>
+                    <asp:TextBox ID="txtHotCategoriaFooter" runat="server" />
+                </FooterTemplate>
+            </asp:TemplateField>
+
+
+            <asp:TemplateField HeaderText="Acciones">
+                <ItemTemplate>
+                    <asp:ImageButton ImageUrl="/Images/edit_icon.png" runat="server" CommandName="Edit" ToolTip="Editar" type="button" />
+                    <asp:ImageButton ImageUrl="/Images/trash_icon.png" runat="server" CommandName="Delete" ToolTip="Eliminar" type="button" />
+                </ItemTemplate>
+                <EditItemTemplate>
+                    <asp:ImageButton ImageUrl="/Images/check.png" runat="server" CommandName="Update" ToolTip="Actualizar" />
+                    <asp:ImageButton ImageUrl="/Images/close.png" runat="server" CommandName="Cancel" ToolTip="Cancelar" />
+                </EditItemTemplate>
+            </asp:TemplateField>
+
+        </Columns>
+
+    </asp:GridView>
+
+    <div class="col-md-6">
+
+        <div class="form-inline mb-3">
+            <asp:Label ID="Label1" runat="server" Text="Pago Código" CssClass="col-form-label col-md-4"></asp:Label>
+            <asp:TextBox ID="txtPagoCodigo" runat="server" CssClass="form-control col-md-8"></asp:TextBox>
+        </div>
+
+        <div class="form-inline mb-3">
+            <asp:Label ID="Label3" runat="server" Text="Comprobante Factura" CssClass="col-form-label col-md-4"></asp:Label>
+            <asp:TextBox ID="txtFacComprobante" runat="server" CssClass="form-control col-md-8"></asp:TextBox>
+        </div>
+
+         <div class="form-inline mb-3">
+            <asp:Label  runat="server" Text="Estado" CssClass="col-form-label col-md-4"></asp:Label>
+            <asp:DropDownList ID="drpDisponibilidad" runat="server" CssClass="form-control col-md-8">
+                <asp:ListItem Selected="True" Value="R">Recibida</asp:ListItem>
+                <asp:ListItem Value="P">Pagada</asp:ListItem>
+             </asp:DropDownList>
+        </div>
+
+        <div class="form-inline mt-3 mb-1">
+            <asp:Button ID="btnAgregar" CssClass="btn btn-primary col-md-12 mt-3" runat="server" Text="Agregar" OnClick="btnAgregar_Click" />
+            <asp:Label ID="lblStatus" runat="server" Text="Label" ForeColor="#006600" Visible="False"></asp:Label>
+        </div>
+
+    </div>
+</asp:Content>
---
+++ AppReservasSW/Views/Factura.aspx.cs
@@ -0,0 +1,248 @@
+﻿using AppReservasSW.Controllers;
+using Microsoft.Ajax.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+
+namespace AppReservasSW.Views
+{
+    public partial class Factura : System.Web.UI.Page
+    {
+        IEnumerable<Models.Factura> facturas = new ObservableCollection<Models.Factura>();
+        FacturaManager facturaManager = new FacturaManager();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                InicializarControles();
+            }
+        }
+
+        private async void InicializarControles()
+        {
+
+            facturas = await facturaManager.ObtenerFacturas(VG.usuarioActual.CadenaToken);
+            grdFacturas.DataSource = facturas.ToList();
+            grdFacturas.DataBind();
+        }
+
+        protected async void btnAgregar_Click(object sender, EventArgs e)
+        {
+            if (ValidarInsertar())
+            {
+                Models.Factura facturaIngresado = new Models.Factura();
+                Models.Factura factura = new Models.Factura()
+                {
+
+                    PAG_CODIGO = Convert.ToInt32(txtPagoCodigo.Text),
+                    FAC_COMPROBANTE = txtFacComprobante.Text,
+                    FAC_ESTADO = drpDisponibilidad.SelectedValue.ToString()
+                };
+
+                facturaIngresado =
+                    await facturaManager.Ingresar(factura, VG.usuarioActual.CadenaToken);
+
+                if (facturaIngresado != null)
+                {
+                    lblStatus.Text = "Factura ingresada correctamente";
+                    lblStatus.ForeColor = Color.Green;
+                    lblStatus.Visible = true;
+                    InicializarControles();
+                }
+                else
+                {
+                    lblStatus.Text = "Hubo un error al ingresar la factura";
+                    lblStatus.ForeColor = Color.Maroon;
+                    lblStatus.Visible = true;
+                }
+            }
+        }
+
+        protected async void grdFacturas_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            string codigoFacturaEliminado = string.Empty;
+            Label lblCode = (Label)grdFacturas.Rows[e.RowIndex].Cells[0].FindControl("lblCodigoFactura");
+
+            string codigoFactura = lblCode.Text;
+
+            codigoFacturaEliminado = await facturaManager.Eliminar(codigoFactura, VG.usuarioActual.CadenaToken);
+
+            if (!string.IsNullOrEmpty(codigoFacturaEliminado))
+            {
+                lblStatus.Text = "Factura eliminada correctamente";
+                lblStatus.ForeColor = Color.Green;
+                lblStatus.Visible = true;
+                InicializarControles();
+            }
+            else
+            {
+                lblStatus.Text = "Hubo un error al eliminar la factura";
+                lblStatus.ForeColor = Color.Maroon;
+                lblStatus.Visible = true;
+            }
+
+        }
+
+        protected void grdFacturas_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            grdFacturas.EditIndex = e.NewEditIndex;
+            InicializarControles();
+        }
+
+        protected void grdFacturas_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            grdFacturas.EditIndex = -1;
+            InicializarControles();
+        }
+
+        protected async void grdFacturas_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            Label lblCode = (Label)grdFacturas.Rows[e.RowIndex].Cells[0].FindControl("lblCodigoFactura");
+
+            string facCodigoPago = (grdFacturas.Rows[e.RowIndex].FindControl("txtPagoCodigoEdit") as TextBox).Text;
+            string facComprobante = (grdFacturas.Rows[e.RowIndex].FindControl("txtFacComprobanteEdit") as TextBox).Text;
+            string facEstado = (grdFacturas.Rows[e.RowIndex].FindControl("drpDisponibilidadEdit") as DropDownList).Text;
+
+            if (ValidarModificar(facCodigoPago, facComprobante, facEstado))
+            {
+                Models.Factura facturaModificado = new Models.Factura();
+                Models.Factura factura = new Models.Factura()
+                {
+                    FAC_CODIGO = Convert.ToInt32(lblCode.Text),
+                    PAG_CODIGO = Convert.ToInt32(facCodigoPago),
+                    FAC_COMPROBANTE = facComprobante,
+                    FAC_ESTADO = facEstado
+                };
+
+                facturaModificado =
+                    await facturaManager.Actualizar(factura, VG.usuarioActual.CadenaToken);
+
+                if (facturaModificado != null)
+                {
+                    lblStatus.Text = "Factura modificada correctamente";
+                    lblStatus.ForeColor = Color.Green;
+                    lblStatus.Visible = true;
+                    InicializarControles();
+                }
+                else
+                {
+                    lblStatus.Text = "Hubo un error al modificar la factura";
+                    lblStatus.ForeColor = Color.Maroon;
+                    lblStatus.Visible = true;
+                }
+
+                grdFacturas.EditIndex = -1;
+                InicializarControles();
+            }
+
+        }
+
+        protected void grdFacturas_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+
+        }
+
+        private bool ValidarInsertar()
+        {
+
+            if (txtPagoCodigo.Text.IsNullOrWhiteSpace())
+            {
+                lblStatus.Text = "Debe ingresar el código del pago";
+                lblStatus.ForeColor = Color.Maroon;
+                lblStatus.Visible = true;
+                return false;
+            }
+
+            if (txtPagoCodigo.Text.All(char.IsNumber) == false)
+            {
+                lblStatus.Text = "El código de pago debe ser un número";
+                lblStatus.ForeColor = Color.Maroon;
+                lblStatus.Visible = true;
+                return false;
+            }
+
+            if (txtFacComprobante.Text.IsNullOrWhiteSpace())
+            {
+                lblStatus.Text = "Debe ingresar el comprobante";
+                lblStatus.ForeColor = Color.Maroon;
+                lblStatus.Visible = true;
+                return false;
+            }
+
+            if (txtFacComprobante.Text.All(char.IsNumber) == false)
+            {
+                lblStatus.Text = "El comprobante debe ser un número";
+                lblStatus.ForeColor = Color.Maroon;
+                lblStatus.Visible = true;
+                return false;
+            }
+
+            if (drpDisponibilidad.SelectedValue.IsNullOrWhiteSpace())
+            {
+                lblStatus.Text = "Debe ingresar el estado de comprobante";
+                lblStatus.ForeColor = Color.Maroon;
+                lblStatus.Visible = true;
+                return false;
+            }
+
+
+            return true;
+        }
+
+        private bool ValidarModificar(string facCodigoPago, string facComprobante, string facEstado)
+        {
+
+            if (facCodigoPago.IsNullOrWhiteSpace())
+            {
+                lblStatus.Text = "Debe ingresar el código del pago";
+                lblStatus.ForeColor = Color.Maroon;
+                lblStatus.Visible = true;
+                return false;
+            }
+
+
+            if (facCodigoPago.All(char.IsNumber) == false)
+            {
+                lblStatus.Text = "El código de pago debe ser un número";
+                lblStatus.ForeColor = Color.Maroon;
+                lblStatus.Visible = true;
+                return false;
+            }
+
+
+            if (facComprobante.IsNullOrWhiteSpace())
+            {
+                lblStatus.Text = "Debe ingresar el comprobante de factura";
+                lblStatus.ForeColor = Color.Maroon;
+                lblStatus.Visible = true;
+                return false;
+            }
+
+            if (facComprobante.All(char.IsNumber) == false)
+            {
+                lblStatus.Text = "El comprobante debe ser un número";
+                lblStatus.ForeColor = Color.Maroon;
+                lblStatus.Visible = true;
+                return false;
+            }
+
+            if (facEstado.IsNullOrWhiteSpace())
+            {
+                lblStatus.Text = "Debe ingresar el estado de factura";
+                lblStatus.ForeColor = Color.Maroon;
+                lblStatus.Visible = true;
+                return false;
+            }
+
+            return true;
+        }
+
+
+    }
+}
